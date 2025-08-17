978b232c8aa43ad7e9f03c3056c9a3c3762d4225
Soumyajit Karforma <97759661+SoumyajitProjects@users.noreply.github.com>
2025-08-17 19:37:44 -0400

    Update solution for middle-of-the-linked-list

--- leetcode/middle-of-the-linked-list.py
+++ leetcode/middle-of-the-linked-list.py
@@ -1,14 +1,19 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#         self.val = val         # Value stored in the node
+#         self.next = next       # Pointer to the next node in the list
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow , fast = head , head
+        # Initialize two pointers: 'slow' and 'fast'
+        slow, fast = head, head
 
+        # Move 'fast' two steps and 'slow' one step at a time
+        # When 'fast' reaches the end, 'slow' will be at the middle
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
         
+        # 'slow' now points to the middle node
         return slow
