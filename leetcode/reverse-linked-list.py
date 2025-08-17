0c8c864bd6328a689ad91c07f27e61bf8c1527d1
Soumyajit Karforma <97759661+SoumyajitProjects@users.noreply.github.com>
2025-08-17 19:26:06 -0400

    Update solution for reverse-linked-list

--- leetcode/reverse-linked-list.py
+++ leetcode/reverse-linked-list.py
@@ -1,29 +1,20 @@
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#         self.val = val         # Value stored in the node
+#         self.next = next       # Pointer to the next node in the list
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize previous pointer as None (end of reversed list)
-        prev = None
 
-        # Start from the head of the original list
-        curr = head
+        prev = None              # 'prev' will eventually become the new head (start as None)
+        curr = head              # 'curr' starts at the current head of the list
 
-        # Iterate through the linked list
+        # Traverse the entire linked list
         while curr:
-            # Save the next node before breaking the link
-            next_node = curr.next
+            forward = curr.next  # Save the next node to move forward later
+            curr.next = prev     # Reverse the link: point current node back to 'prev'
+            prev = curr          # Move 'prev' one step forward (to current node)
+            curr = forward       # Move 'curr' one step forward (to saved 'forward')
 
-            # Reverse the current node's pointer
-            curr.next = prev
-
-            # Move the 'prev' pointer one step forward (to current)
-            prev = curr
-
-            # Move to the next node in the original list
-            curr = next_node
-
-        # At the end, prev is the new head of the reversed list
-        return prev
+        return prev              # At the end, 'prev' will be the new head of the reversed list
