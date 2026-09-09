58bfb64963c5fae23416d9f2bd8da3927b18ab25
Soumyajit Karforma <97759661+SoumyajitProjects@users.noreply.github.com>
2026-09-08 20:46:07 -0400

    Update solution for valid-parentheses

--- leetcode/valid-parentheses.py
+++ leetcode/valid-parentheses.py
@@ -1,25 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        # Initialize an empty stack to keep track of opening brackets
         stack = []
+        closeToOpen = {")" : "(", "]" : "[", "}" : "{"}
 
-        # Mapping of closing brackets to their corresponding opening brackets
-        CloseToOpen = {")": "(", "]": "[", "}": "{"}
-
-        # Iterate through each character in the input string
         for c in s:
-            # If the character is a closing bracket
-            if c in CloseToOpen:
-                # Check if the stack is not empty and the top of the stack matches the corresponding opening bracket
-                if stack and stack[-1] == CloseToOpen[c]:
-                    stack.pop()  # Valid pair found, pop the opening bracket
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
                 else:
-                    return False  # Invalid pair or unmatched closing bracket
+                    return False
             else:
-                # If it's an opening bracket, push it onto the stack
                 stack.append(c)
-
-        # After processing all characters, the stack should be empty for a valid string
-        if not stack:
-            return True  # All brackets matched
-        return False  # Unmatched opening brackets remain
+        return True if not stack else False
